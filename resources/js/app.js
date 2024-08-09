862f937b5e5425e63a6da392ff3eb48841cf17f1
Ameer Hamza <ameer.hamza@codeforesight.com>
2024-08-09 11:47:55 +0500

    Cart Management
    Implemented Complete Cart functionalities on frontend and backend for local and in database
    Implemented cart functionality for guest user and login user

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,122 +2,93 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse'
+import { post } from './http';
 
 Alpine.plugin(collapse)
 
 window.Alpine = Alpine;
 
 document.addEventListener("alpine:init", () => {
-  Alpine.store("header", {
-    cartItemsObject: {},
-    watchingItems: [],
-    get watchlistItems() {
-      return this.watchingItems.length;
-    },
-    get cartItems() {
-      return Object.values(this.cartItemsObject).reduce(
-        (accum, next) => accum + parseInt(next.quantity),
-        0
-      );
-    },
-  });
 
-  Alpine.data("toast", () => ({
-    visible: false,
-    delay: 5000,
-    percent: 0,
-    interval: null,
-    timeout: null,
-    message: null,
-    close() {
-      this.visible = false;
-      clearInterval(this.interval);
-    },
-    show(message) {
-      this.visible = true;
-      this.message = message;
+    Alpine.data("toast", () => ({
+        visible: false,
+        delay: 5000,
+        percent: 0,
+        interval: null,
+        timeout: null,
+        message: null,
+        close() {
+            this.visible = false;
+            clearInterval(this.interval);
+        },
+        show(message) {
+            this.visible = true;
+            this.message = message;
 
-      if (this.interval) {
-        clearInterval(this.interval);
-        this.interval = null;
-      }
-      if (this.timeout) {
-        clearTimeout(this.timeout);
-        this.timeout = null;
-      }
+            if (this.interval) {
+                clearInterval(this.interval);
+                this.interval = null;
+            }
+            if (this.timeout) {
+                clearTimeout(this.timeout);
+                this.timeout = null;
+            }
 
-      this.timeout = setTimeout(() => {
-        this.visible = false;
-        this.timeout = null;
-      }, this.delay);
-      const startDate = Date.now();
-      const futureDate = Date.now() + this.delay;
-      this.interval = setInterval(() => {
-        const date = Date.now();
-        this.percent = ((date - startDate) * 100) / (futureDate - startDate);
-        if (this.percent >= 100) {
-          clearInterval(this.interval);
-          this.interval = null;
-        }
-      }, 30);
-    },
-  }));
+            this.timeout = setTimeout(() => {
+                this.visible = false;
+                this.timeout = null;
+            }, this.delay);
+            const startDate = Date.now();
+            const futureDate = Date.now() + this.delay;
+            this.interval = setInterval(() => {
+                const date = Date.now();
+                this.percent = ((date - startDate) * 100) / (futureDate - startDate);
+                if (this.percent >= 100) {
+                    clearInterval(this.interval);
+                    this.interval = null;
+                }
+            }, 30);
+        },
+    }));
 
-  Alpine.data("productItem", (product) => {
-    return {
-      id: product.id,
-      product,
-      quantity: 1,
-      get watchlistItems() {
-        return this.$store.watchlistItems;
-      },
-      addToWatchlist() {
-        if (this.isInWatchlist()) {
-          this.$store.header.watchingItems.splice(
-            this.$store.header.watchingItems.findIndex(
-              (p) => p.id === product.id
-            ),
-            1
-          );
-          this.$dispatch("notify", {
-            message: "The item was removed from your watchlist",
-          });
-        } else {
-          this.$store.header.watchingItems.push(product);
-          this.$dispatch("notify", {
-            message: "The item was added into the watchlist",
-          });
-        }
-      },
-      isInWatchlist() {
-        return this.$store.header.watchingItems.find(
-          (p) => p.id === product.id
-        );
-      },
-      addToCart(id, quantity = 1) {
-        this.$store.header.cartItemsObject[id] = this.$store.header
-          .cartItemsObject[id] || {...product, quantity: 0};
-        this.$store.header.cartItemsObject[id].quantity =
-          parseInt(this.$store.header.cartItemsObject[id].quantity) + parseInt(quantity);
-        this.$dispatch("notify", {
-          message: "The item was added into the cart",
-        });
-      },
-      removeItemFromCart() {
-        delete this.$store.header.cartItemsObject[this.id];
-        this.$dispatch("notify", {
-          message: "The item was removed from cart",
-        });
-      },
-      removeFromWatchlist() {
-        this.$store.header.watchingItems.splice(
-          this.$store.header.watchingItems.findIndex((p) => p.id === this.id),
-          1
-        );
-      },
-    };
-  });
-  
+    Alpine.data("productItem", (product) => {
+        return {
+            product,
+
+            addToCart(quantity = 1) {
+                post(this.product.addToCartUrl, { quantity })
+                    .then(result => {
+                        this.$dispatch('cart-change', { count: result.count })
+                        this.$dispatch("notify", {
+                            message: "The item was added into the cart",
+                        });
+                    })
+                    .catch(response => {
+                        console.log(response);
+                    })
+            },
+            removeItemFromCart() {
+                post(this.product.removeUrl)
+                    .then(result => {
+                        this.$dispatch("notify", {
+                            message: "The item was removed from cart",
+                        });
+                        this.$dispatch('cart-change', { count: result.count })
+                        this.cartItems = this.cartItems.filter(p => p.id !== product.id)
+                    })
+            },
+            changeQuantity() {
+                post(this.product.updateQuantityUrl, { quantity: product.quantity })
+                    .then(result => {
+                        this.$dispatch('cart-change', { count: result.count })
+                        this.$dispatch("notify", {
+                            message: "The item quantity was updated",
+                        });
+                    })
+            },
+        };
+    });
+
 });
 
 
